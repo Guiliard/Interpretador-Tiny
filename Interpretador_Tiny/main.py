a44823cea3b212418e2a71c3b75a6359251c9250
Guiliard <camposrafa806@gmail.com>
2023-10-25 12:12:45 -0300

    Test new file pow_ot.tiny

--- Interpretador_Tiny/main.py
+++ Interpretador_Tiny/main.py
@@ -3,7 +3,7 @@
 from Parser import Parser
 
 def main ():
-    nome_arq = 'Testes_Interpretador/somatorio.tiny'
+    nome_arq = 'Testes_Interpretador/pow_ot.tiny'
     Interpretador = Parser()
     object_interpretador = Interpretador.inicio(nome_arq)
     object_interpretador.executar()
